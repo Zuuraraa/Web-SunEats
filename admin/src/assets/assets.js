0c90adbaaa08d377f971b742714eddcd85d1542e
Angel Felicia <feliciaangel499@gmail.com>
2025-05-21 10:03:55 +0700

    ganti path assets

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,9 +1,15 @@
-const assets = {
-    logo_admin: '/src/assets/Logo_admin.png',
-    user: '/src/assets/User.png',
-    logo_noBG: '/src/assets/Logo_noBG.png',
-    upload: '/src/assets/Upload.png',
-    logout: '/src/assets/Logout.png'
-}
+import logo_admin from './Logo_admin.png';
+import user from './User.png';
+import logo_noBG from './Logo_noBG.png';
+import upload from './Upload.png';
+import logout from './Logout.png';
 
-export {assets}
+const assets = {
+    logo_admin,
+    user,
+    logo_noBG,
+    upload,
+    logout
+};
+
+export { assets };
